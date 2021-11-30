dafc583a2160b46e2a22510221b9abc938e9c73a
Alexandr Fetisov <fetis_94@mail.ru>
2021-11-30 16:59:37 +0500

    deploy heroku

--- server/utils/sequelizeConfig.js
+++ server/utils/sequelizeConfig.js
@@ -6,9 +6,12 @@ const {Sequelize} = require('sequelize')
 // });
 
 
-const sequelize = new Sequelize('d295jo865l0vcm', 'hczqeczggcokaa', '8774cbf35830d5164a854102551b7a748af25272c5bc1cff22e4e0a598aa854a', {
-    host: 'ec2-54-74-60-70.eu-west-1.compute.amazonaws.com',
-    dialect: 'postgres'
-});
+const sequelize = new Sequelize('postgres://hczqeczggcokaa:8774cbf35830d5164a854102551b7a748af25272c5bc1cff22e4e0a598aa854a@ec2-54-74-60-70.eu-west-1.compute.amazonaws.com:5432/d295jo865l0vcm', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+})
 
 module.exports = sequelize
