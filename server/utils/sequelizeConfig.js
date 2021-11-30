c62d08b84ac92e76a24d95d4f29555d729981d63
Alexandr Fetisov <fetis_94@mail.ru>
2021-11-30 17:15:45 +0500

    new

--- server/utils/sequelizeConfig.js
+++ server/utils/sequelizeConfig.js
@@ -1,20 +1,8 @@
 const {Sequelize} = require('sequelize')
 
-// const sequelize = new Sequelize('todos', 'postgres', 'postgres', {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
-
-
-const sequelize = new Sequelize('postgres://hczqeczggcokaa:8774cbf35830d5164a854102551b7a748af25272c5bc1cff22e4e0a598aa854a@ec2-54-74-60-70.eu-west-1.compute.amazonaws.com:5432/d295jo865l0vcm', {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
+const sequelize = new Sequelize('todos', 'postgres', 'postgres', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
 
 module.exports = sequelize
